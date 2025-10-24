180d1e217bba01a1a250191213ff92e52186eb4b
George <g.dedej@prodacta.it>
2025-10-24 09:47:26 +0200

    feat: update about page and menu

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { MenuComponent } from '../menu/menu.component';
 
 @Component({
@@ -6,63 +6,26 @@ import { MenuComponent } from '../menu/menu.component';
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent extends MenuComponent{
-  // @ViewChild('canvas', { static: true }) canvasRef: ElementRef;
-  // private canvas: HTMLCanvasElement;
-  // private ctx: CanvasRenderingContext2D;
-  // private requestId: number;
-  // private t: number = 0;
-  // private inc: number = 3;
-  // private stopped: boolean = true;
+export class AboutComponent implements OnInit, OnDestroy {
 
-  // constructor() {}
 
-  // ngOnInit(): void {
-  //   this.canvas = this.canvasRef.nativeElement;
-  //   this.ctx = this.canvas.getContext('2d');
-  //   this.initCanvas();
-  //   this.startAnimation();
-  // }
+  constructor() {}
 
-  // ngOnDestroy(): void {
-  //   cancelAnimationFrame(this.requestId);
-  // }
+  ngOnInit(): void {
 
-  // initCanvas(): void {
-  //   // Initialize your canvas here
-  //   // Make sure to create the canvas and set its initial properties
-  // }
+  }
 
-  // draw(): void {
-  //   // Implement your drawing logic here
-  // }
+  ngOnDestroy(): void {
+  }
 
-  // startAnimation(): void {
-  //   this.stopped = !this.stopped;
-  //   if (!this.stopped) {
-  //     this.animate();
-  //   }
-  // }
+  initCanvas(): void {
+    // Initialize your canvas here
+    // Make sure to create the canvas and set its initial properties
+  }
 
-  // animate(): void {
-  //   if (this.stopped) {
-  //     return;
-  //   }
-  //   this.t += this.inc;
-  //   if (!this.draw() || this.t < 0) {
-  //     if (this.inc === 3) {
-  //       this.inc = -8;
-  //     } else {
-  //       this.ctx.strokeStyle = `hsla(${this.getRandomInt(0, 360)}, 90%, 60%, 0.6)`;
-  //       this.inc = 3;
-  //       this.t = 0;
-  //       // Reset and reinitialize your canvas if needed
-  //     }
-  //   }
-  //   this.requestId = requestAnimationFrame(() => this.animate());
-  // }
+  draw(): void {
+    // Implement your drawing logic here
+  }
 
-  // getRandomInt(min: number, max: number): number {
-  //   return Math.floor(Math.random() * (max - min)) + min;
-  // }
+  
 }
